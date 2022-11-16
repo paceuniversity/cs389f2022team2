18a25494e6ce97bddb2d0fa4ccc6aad418632433
Joseph Indriolo <joe.indriolo@gmail.com>
2022-11-15 23:47:11 -0500

    full login/signup functionality, workoutobject closes #28, #32, #34, #33, #43, #44

--- WellnessWatch/app/src/main/java/com/example/wellnesswatch/LoginActivity.java
+++ WellnessWatch/app/src/main/java/com/example/wellnesswatch/LoginActivity.java
@@ -1,28 +1,52 @@
 package com.example.wellnesswatch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText inputEmail, inputPassword;
+    Button btnLogin;
+    ProgressDialog progressDialog;
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         this.getSupportActionBar().hide();
 
-        Button login = (Button) findViewById(R.id.loginButton);
-        login.setOnClickListener(new View.OnClickListener() {
+        inputEmail = findViewById(R.id.inputEmailLogin);
+        inputPassword = findViewById(R.id.inputPasswordLogin);
+        btnLogin = findViewById(R.id.loginButton);
+        progressDialog = new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(i);
+                LogInUser();
             }
         });
 
@@ -35,4 +59,45 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void LogInUser() {
+        String email = inputEmail.getText().toString();
+        String password = inputPassword.getText().toString();
+
+
+        if (email.isEmpty() || !isValidEmail(email)) {
+            inputEmail.setError("Please enter a valid email.");
+        } else if (password.isEmpty()) {
+            inputPassword.setError("Please enter a valid password.");
+        } else {
+            progressDialog.setMessage("Logging In...");
+            progressDialog.setTitle("Log in");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()) {
+                        progressDialog.dismiss();
+                        Toast.makeText(LoginActivity.this, "Log in successful", Toast.LENGTH_SHORT).show();
+                        sendUserToNextActivity();
+                    }else{
+                        progressDialog.dismiss();
+                        Toast.makeText(LoginActivity.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+        }
+    }
+    private Boolean isValidEmail(CharSequence target) {
+        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
+    }
+
+    private void sendUserToNextActivity() {
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(i);
+    }
+
 }
--- WellnessWatch/app/src/main/java/com/example/wellnesswatch/SignUpActivity.java
+++ WellnessWatch/app/src/main/java/com/example/wellnesswatch/SignUpActivity.java
@@ -1,28 +1,120 @@
 package com.example.wellnesswatch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
+    EditText inputEmail, inputName, inputPassword, inputConformPassword, inputPhone;
+    Button btnRegister;
+    ProgressDialog progressDialog;
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         this.getSupportActionBar().hide();
+        inputName = findViewById(R.id.inputName);
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
+        inputConformPassword = findViewById(R.id.inputConfirmPassword);
+        inputPhone = findViewById(R.id.inputPhone);
+        btnRegister = findViewById(R.id.signupButton);
+        progressDialog = new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
 
-        Button login = (Button) findViewById(R.id.signupButton);
-        login.setOnClickListener(new View.OnClickListener() {
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(i);
+                ValidateDetails();
+
             }
         });
 
+    }
+
+    private void ValidateDetails() {
+        String name = inputName.getText().toString();
+        String email = inputEmail.getText().toString();
+        String password = inputPassword.getText().toString();
+        String confirmPassword = inputConformPassword.getText().toString();
+        String phone = inputPhone.getText().toString();
+
+
+        if (name.isEmpty()) {
+            inputName.setError("Please enter your name");
+
+        } else if (!isValidEmail(email)) {
+            inputEmail.setError("Please enter a valid email");
+        } else if (password.isEmpty() || password.length() < 6) {
+            inputPassword.setError("Please enter a valid password. Password must be longer then 6 characters.");
+        } else if (!password.equals(confirmPassword)) {
+            inputConformPassword.setError("Your password does not match.");
+        } else if (phone.isEmpty()) {
+            inputPhone.setError("Please enter a phone number.");
+        } else {
+            progressDialog.setMessage("Please wait...");
+            progressDialog.setTitle("Registration");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        User user = new User(name, email, phone);
+                        FirebaseDatabase.getInstance().getReference("Users")
+                                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+                                            Toast.makeText(SignUpActivity.this, "User has been registered", Toast.LENGTH_LONG).show();
+                                            progressDialog.dismiss();
+                                            sendUserToNextActivity();
+                                        } else {
+                                            Toast.makeText(SignUpActivity.this, "Failed to register", Toast.LENGTH_LONG).show();
+                                        }
+                                    }
+                                });
+                    }else{
+                        Toast.makeText(SignUpActivity.this, "Failed to register", Toast.LENGTH_LONG).show();
+                        progressDialog.dismiss();
+                    }
+                }
+            });
+        }
+
+
+    }
+
+    private void sendUserToNextActivity() {
+        Intent i = new Intent(SignUpActivity.this, LoginActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(i);
+    }
+
+    private Boolean isValidEmail(CharSequence target) {
+        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
     }
 }
