c4261b4479862b2008b8efbaeab0cee099680d63
sanaya863 <sanayadeas951@gmail.com>
2022-12-06 12:38:59 -0500

    Created About Us and Log Out activities, modified Settings menu

--- WellnessWatch/app/src/main/java/com/example/wellnesswatch/MainActivity.java
+++ WellnessWatch/app/src/main/java/com/example/wellnesswatch/MainActivity.java
@@ -5,13 +5,13 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.app.ActivityManager;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.wellnesswatch.databinding.ActivityMainBinding;
@@ -73,7 +73,28 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
         switch(id) {
             case R.id.settingsmenu:
-                //Switch to settings activity on click...
+                Button settings = (Button) findViewById(R.id.settingsmenu);
+                settings.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
+                    }
+                });
+
+                break;
+            case R.id.aboutusmenu:
+                Button about = (Button) findViewById(R.id.aboutusmenu);
+                about.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startActivity(new Intent(getApplicationContext(), AboutUsActivity.class));
+                    }
+                });
+                break;
+            case R.id.logoutmenu:
+                Toast.makeText(getApplicationContext(), "You've been logged out!",Toast.LENGTH_LONG).show();
+                //placeholder toast popup, not sure if this will need to be an activity atm
+                //Switch to log out activity on click...
                 break;
         }
         return true;
